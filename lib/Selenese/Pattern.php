5c514f6006b7069091475f2ea5a29089fda57934
chuyskywalker <github.com@jrm.cc>
2013-06-09 18:53:39 -0700

    Use regex instead of fnmatch because it breaks over 4k characters; better pattern method detection

--- lib/Selenese/Pattern.php
+++ lib/Selenese/Pattern.php
@@ -10,7 +10,7 @@ class Pattern {
     // todo: support comma separated lists of values
     public function __construct($pattern) {
         $explode = explode(':', $pattern, 2);
-        if (count($explode) == 2) {
+        if (count($explode) == 2 && in_array($explode[0], array('glob', 'regex', 'regexp', 'regexpi', 'exact'))) {
             $this->type = $explode[0];
             $this->value = $explode[1];
         }
@@ -28,7 +28,13 @@ class Pattern {
     public function match($content) {
         switch ($this->type) {
             case 'glob':
-                return fnmatch($this->value, $content);
+                // convert the glob to a regex, cause glob will choke on long strings, like the entire source of a page
+                $regex = str_replace(
+                    array("\*", "\?"), // wildcard chars
+                    array('.*','.'),   // regexp chars
+                    preg_quote($this->value)
+                );
+                return (bool) preg_match('/' . $regex .  '/U', $content);
                 break;
             case 'regexpi':
                 $flags = 'i';
