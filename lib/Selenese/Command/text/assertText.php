f21eef3e345efadcf1d3eb92dc852f97721bafa5
chuyskywalker <github.com@jrm.cc>
2013-06-15 16:24:42 -0700

    Remove redundant exception catching -- it will move up to the runner

--- lib/Selenese/Command/text/assertText.php
+++ lib/Selenese/Command/text/assertText.php
@@ -6,12 +6,7 @@ namespace Selenese\Command;
 class assertText extends Command {
     public function runWebDriver(\WebDriverSession $session)
     {
-        try {
-            $elementText = $this->getElement($session, $this->arg1)->text();
-            return $this->assert($elementText, $this->arg2);
-        }
-        catch (\Exception $e) {
-            return $this->commandResult(false, false, 'Error: ' . $e->getMessage());
-        }
+        $elementText = $this->getElement($session, $this->arg1)->text();
+        return $this->assert($elementText, $this->arg2);
     }
 }
--- lib/Selenese/Command/text/verifyNotText.php
+++ lib/Selenese/Command/text/verifyNotText.php
@@ -6,12 +6,7 @@ namespace Selenese\Command;
 class verifyNotText extends Command {
     public function runWebDriver(\WebDriverSession $session)
     {
-        try {
-            $elementText = $this->getElement($session, $this->arg1)->text();
-            return $this->verifyNot($elementText, $this->arg2);
-        }
-        catch (\Exception $e) {
-            return $this->commandResult(false, false, 'Error: ' . $e->getMessage());
-        }
+        $elementText = $this->getElement($session, $this->arg1)->text();
+        return $this->verifyNot($elementText, $this->arg2);
     }
 }
