95f0364562ab150b00ca0cbc5d74dc841bfd84a7
chuyskywalker <github.com@jrm.cc>
2013-06-15 16:33:56 -0700

    Reducing boiler plate

--- lib/Selenese/Command/deleteAllVisibleCookies.php
+++ lib/Selenese/Command/deleteAllVisibleCookies.php
@@ -5,12 +5,7 @@ namespace Selenese\Command;
 class deleteAllVisibleCookies extends Command {
     public function runWebDriver(\WebDriverSession $session)
     {
-        try {
-            $session->deleteAllCookies();
-            return $this->commandResult(true, true, 'All cookies deleted');
-        } catch (\Exception $e) {
-            return $this->commandResult(false, false, 'Could not delete cookies. Error: ' . $e->getMessage());
-        }
-
+        $session->deleteAllCookies();
+        return $this->commandResult(true, true, 'All cookies deleted');
     }
 }
